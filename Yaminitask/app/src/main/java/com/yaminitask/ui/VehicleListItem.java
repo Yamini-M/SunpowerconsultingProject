1c2f1176b5bdc4ada007cd55f066e8b82b611666
Yamini Lakshmi <yamini7315@gmail.com>
2016-06-28 19:21:50 +0100

    manufacturer images from external site
    Signed-off-by: Yamini Lakshmi <yamini7315@gmail.com>

--- Yaminitask/app/src/main/java/com/yaminitask/ui/VehicleListItem.java
+++ Yaminitask/app/src/main/java/com/yaminitask/ui/VehicleListItem.java
@@ -6,6 +6,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.yaminitask.R;
 import com.yaminitask.model.Make;
 
@@ -33,16 +34,15 @@ public class VehicleListItem extends LinearLayout {
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        //TODO initialize
         supplierImage = (ImageView) findViewById(R.id.img_supplier_logo);
         carNameTextView = (TextView) findViewById(R.id.vehicle_name);
         manufacturedYear = (TextView) findViewById(R.id.year_manufactured);
     }
 
     public void populateViewWith(Context context, Make make){
-//        String supplierLogoUrl = inventory.getMedia().getPhotos().getThumbnails().getLinks().get(0).getHref();
-//        Picasso.with(context).load(supplierLogoUrl).into(supplierImage);
-        carNameTextView.setText(make.getName());
+        String supplierLogoUrl = "https://logo.clearbit.com/"+make.getName().toLowerCase()+".com";
+        Picasso.with(context).load(supplierLogoUrl).into(supplierImage);
+        carNameTextView.setText(make.getModels().get(0).getId().replaceAll("_", " "));
         manufacturedYear.setText("Manufactured year: "+make.getModels().get(0).getYears().get(0).getYear());
     }
 }
